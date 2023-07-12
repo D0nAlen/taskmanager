d7c2c66e3a16a6ad6c72b59fdaca3682402bf65c
Alena <vasilevnalena.2012@gmail.com>
2023-07-12 22:45:45 +0400

    Added scripts in folder "components"

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require(`path`);
 
 module.exports = {
-  mode: `development`, //режим сборки
-  entry: `./src/main.js`, //точка входа приложения
-  output: { //настройка выходного файла
-    filename: `bundle.js`,
-    path: path.join(__dirname,`public`),
+  mode: "development", //режим сборки
+  entry: "./src/main.js", //точка входа приложения
+  output: {
+    //настройка выходного файла
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
   },
-  devtool: `source-map`, //подключаем sourcemaps'
+  devtool: "source-map", //подключаем sourcemaps
   devServer: {
-    contentBase: path.join(__dirname, `public`),
-    watchContentBase: true
+    contentBase: path.join(__dirname, "public"),
+    // watchContentBase: true,
   }
 };
