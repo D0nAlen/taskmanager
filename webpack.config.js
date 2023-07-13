344ce20bc5c051d7c5e932aec39e2b227fa2b078
Alena <vasilevnalena.2012@gmail.com>
2023-07-13 11:30:37 +0400

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require(`path`);
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
+  mode: `development`,
+  entry: `./src/main.js`,
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "public"),
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`),
   },
-  devtool: "source-map",
+  devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, "public"),
-    // watchContentBase: true,
+    static: path.join(__dirname, `public`),
   },
 };
