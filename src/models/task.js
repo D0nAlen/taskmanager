6ce417547996d847ad1867d8e1cb416cbe98cbc8
Alena <vasilevnalena.2012@gmail.com>
2024-03-27 09:42:19 +0400

    Updated dueDate to tasks(WIP).

--- src/models/task.js
+++ src/models/task.js
@@ -1,18 +1,25 @@
 export default class Task {
     constructor(data) {
-        this.id = data[`id`];
-        this.description = data[`description`] || ``;
-        this.dueDate = data[`due_date`] ? new Date(data[`due_date`]) : null;
-        this.repeatingDays = data[`repeating_days`];
-        this.color = data[`color`];
-        this.isFavorite = Boolean(data[`is_favorite`]);
-        this.isArchive = Boolean(data[`is_archive`]);
-        // this.isArchive = Boolean(data[`is_archived`]);
-        
-        console.log( data[`color`]); //работает
-        console.log( data[`due_date`]); //все "составные_имена", не работают
+        // this.id = data[`id`];
+        // this.description = data[`description`] || ``;
+        // this.dueDate = data[`due_date`] ? new Date(data[`due_date`]) : null;
+        // this.repeatingDays = data[`repeating_days`];
+        // this.color = data[`color`];
+        // this.isFavorite = Boolean(data[`is_favorite`]);
+        // this.isArchive = Boolean(data[`is_archive`]);
+
+        this.id = data.id;
+        this.description = data.description;
+        this.dueDate = data.dueDate;
+        this.repeatingDays = data.repeatingDays;
+        this.color = data.color;
+        this.isFavorite = data.isFavorite;
+        this.isArchive = data.isArchive;
+
+        // console.log(data);
     }
 
+    // 1)переделать!
     toRAW() {
         return {
             "id": this.id,
@@ -26,7 +33,7 @@ export default class Task {
     }
 
     static parseTask(data) {
-        console.log(data);
+        // console.log(data);
         return new Task(data);
     }
 
